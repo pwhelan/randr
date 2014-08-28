1906ca923edc3688ace368b182b6816eb50b5bfe
Phillip Whelan <pwhelan@mixxx.org>
2014-08-28 06:47:19 -0700

    Use Stream I/O exclusively to communicate jobs to permanent workers.

--- src/Worker/Unit.php
+++ src/Worker/Unit.php
@@ -13,35 +13,72 @@ class Unit extends \Evenement\EventEmitter
 {
 	public $permanent = false;
 	public $pid;
+	public $ttl = -1;
 	private $sockets = [];
 	
 	
-	private function _start_permanent($loop)
+	private function _start_permanent(array $options)
 	{
-		socket_create_pair(AF_UNIX, SOCK_STREAM, 0, $this->sockets);
+		$this->sockets = stream_socket_pair(STREAM_PF_UNIX, STREAM_SOCK_STREAM, STREAM_IPPROTO_IP);
+		
+		stream_set_blocking($this->sockets[0], 0);
+		stream_set_blocking($this->sockets[1], 0);
+		
+		stream_set_chunk_size($this->sockets[0], 512);
+		stream_set_chunk_size($this->sockets[1], 512);
 		
 		
 		$pid = pcntl_fork();
 		switch($pid) {
 			case 0:
-				$this->emit('fork');
-				while(true)
+				$this->pid = getmypid();
+				
+				if (array_key_exists('onFork', $options) && is_callable($options['onFork']))
 				{
-					$loop = \React\EventLoop\Factory::create();
-					$pcntl = new \MKraemer\ReactPCNTL\PCNTL($loop);
-					$pcntl->on(SIGUSR1, function() {
-						$l = unpack("llen", socket_read($this->sockets[1], 4));
-						$b = socket_read($this->sockets[1], $l['len']);
-						$job = unserialize($b);
+					$cb = $options['onFork'];
+					$cb($this);
+				}
+				
+				$loop = \React\EventLoop\Factory::create();
+				
+				
+				$loop->addReadStream($this->sockets[0], function() {
+					
+					do
+					{
+						$buf = fread($this->sockets[0], 512);
+						if (!$buf)
+						{
+							break;
+						}
 						
+						$header = (object)unpack("lpackets/llen", $buf);
+						for ($i = 1; $i < $header->packets; $i++)
+						{
+							$buf .= fread($this->sockets[0], 512);
+						}
+						
+						$job = unserialize(substr($buf, 8, $header->len));
 						
 						$rc = $job->perform();
-						
 						$this->emit('finish', [$rc]);
-					});
+						
+						if ($this->ttl > 0)
+						{
+							$this->ttl--;
+						}
+						
+						if ($this->ttl == 0)
+						{
+							exit(0);
+						}
+						
+					} while($buf);
 					
-					$loop->run();
-				}
+				});
+				
+				$loop->run();
+				
 				break;
 			case -1:
 				$this->emit('error');
@@ -53,12 +90,19 @@ class Unit extends \Evenement\EventEmitter
 		}
 	}
 	
-	public function __construct($loop, $permanent = false)
+	public function __construct($permanent = false, array $options = [])
 	{
 		$this->permanent = (bool)$permanent;
+		
+		
+		if (array_key_exists('ttl', $options))
+		{
+			$this->ttl = $options['ttl'];
+		}
+		
 		if ($this->permanent)
 		{
-			$this->_start_permanent($loop);
+			$this->_start_permanent($options);
 		}
 	}
 	
@@ -67,8 +111,15 @@ class Unit extends \Evenement\EventEmitter
 		if ($this->permanent)
 		{
 			$serialized = serialize($job);
-			socket_write($this->sockets[0], pack("l", strlen($serialized)) . $serialized);
-			posix_kill($this->pid, SIGUSR1);
+			$packets = round((strlen($serialized)+8) / 512) + 1;
+			$padding = (512 * $packets) - (strlen($serialized)+8);
+			
+			$rc = fwrite(
+				$this->sockets[1],
+				pack("ll", $packets, strlen($serialized)) .
+				$serialized . str_repeat("A", $padding)
+			);
+			fflush($this->sockets[1]);
 			
 			return;
 		}
